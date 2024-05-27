577b5c2d77e98d07332f75548424c312ed0f3cf5
DaniloDuque <dduque@estudiantec.cr>
2024-05-26 22:35:19 -0600

    ...

--- Genetic-Programming/Symbolic-Regression/code/Graphic.rkt
+++ Genetic-Programming/Symbolic-Regression/code/Graphic.rkt
@@ -1,10 +1,28 @@
 #lang racket
 
-(require "util.rkt" "input.rkt" plot racket/gui)
+(require "util.rkt" plot racket/gui)
 (provide init update)
 
 (define exp '())
-(define frame_ (new frame% [label "Aproximated Function"] [width 600] [height 420]))
+(define frame_ (new frame% [label "Aproximated Function"] [width 800] [height 600]))
+
+(define (match fun x y)
+    (cond
+        ((eq? fun '_+_) (+ x y))
+        ((eq? fun '_-_) (- x y))
+        ((eq? fun '_*_) (* x y))
+        ((eq? fun '_/_) (/ x y))
+        ((eq? fun '_^_) (expt x y))
+        (#t (log y x))
+    )
+)
+
+(define (eval-parse fun x y)
+    (cond
+        ((not (list? fun)) (cond ((eq? fun 'x) x) ((eq? fun 'y) y) (#t fun)))
+        (#t (match (car fun) (eval-parse (cadr fun) x y) (eval-parse (caddr fun) x y)))
+    )
+)
 
 (define canvas_
     (new canvas%
@@ -13,10 +31,10 @@
             (lambda (canvas dc)
                 (send dc draw-bitmap
                 (plot3d-bitmap (list
-                (surface3d (λ (x y) (eval-exp exp x y)) -10 25 -10 25)
-                (points3d entry #:sym 'dot #:size 30 #:color 3))
-                #:width 600
-                #:height 420)
+                (surface3d (λ (x y) (eval-parse exp x y)) -10 25 -10 25)
+                (points3d entry #:sym 'dot #:size 30 #:color 0))
+                #:width 800
+                #:height 600)
                 0 0)
             )
         ]
