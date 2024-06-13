b3639bfa99aac4b8846a02a2c794512426e67a5f
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-06-13 11:57:36 -0600

    Delete Genetic-Programming/Symbolic-Regression/code/functions/script.py

--- Genetic-Programming/Symbolic-Regression/code/functions/script.py
+++
@@ -1,8 +0,0 @@
-import math
-from random import randint
-
-def f(x, y): return pow(2, (x*x + y*y))
-for i in range(20):
-    x = randint(-5, 5)
-    y = randint(-5, 5)
-    print(f"({x} {y} {f(x, y)})")
